c832dc7876dcc74a33f646c5de159cb9cb324dfc
amkahn <amkahn@uw.edu>
2014-06-03 14:26:16 -0700

    Fix encoding issue in process_quotes script

--- src/process_quotes.py
+++ src/process_quotes.py
@@ -21,19 +21,35 @@ def main():
     out_file = open(out_file, 'w')
     
     for line in in_file:
-        line_elements = line.split('\t')
+        line_elements = line.strip().split('\t')
         if len(line_elements) != 4:
             stderr.write("Warning: Input file line not in expected format: %s\n" % line_elements)
             stderr.write("Skipping line\n")
         else:
 #           stderr.write("DEBUG  Here is the quote before processing: %s\n" % line_elements[1])
-            quote = line_elements[1].lower()
 #           stderr.write(str(type(quote))+'\n') # should be unicode
-#           quote = quote.encode('UTF-8')
+            id = line_elements[0].decode('UTF-8')
+            source = line_elements[2].decode('UTF-8')
+            target = line_elements[3].decode('UTF-8')
+
+#           try:
+            quote = line_elements[1].decode('UTF-8')
+#           except UnicodeDecodeError:
+#               stderr.write("Cannot decode quote: %s\n" % quote)
+
+            quote = quote.lower()
             tokens = [word for sent in sent_tokenize(quote) for word in word_tokenize(sent)]
             quote = ' '.join(tokens)
 #           stderr.write("DEBUG  Here is the processed quote: %s\n" % quote)
-            out_file.write(line_elements[0]+'\t'+quote+'\t'+line_elements[2]+'\t'+line_elements[3])
+            out_file.write(id.encode('UTF-8')+'\t')
+
+#           try:
+            out_file.write(quote.encode('UTF-8')+'\t')
+#           except UnicodeEncodeError:
+#               stderr.write("Cannot decode quote: %s\n" % quote)
+
+            out_file.write(source.encode('UTF-8')+'\t')
+            out_file.write(target.encode('UTF-8')+'\n')
     
     in_file.close()
     out_file.close()
